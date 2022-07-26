ba0ab852cb4c603e591d490964d83f5b370bfbef
tepumasuta <thinkingocean2@gmail.com>
2022-07-26 19:37:41 +0300

    Implemented Database constructor and `name` & `path` properties

--- src/database.py
+++ src/database.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 from dataclasses import dataclass, field, asdict
 from serialize import *
 
@@ -27,3 +28,27 @@ class Record(ISerializable):
             self.last_update_date,
             self.repeated_times,
         )
+
+
+class Database:
+    def __init__(self, path: pathlib.Path, serializer: ISerializer) -> None:
+        if not os.path.exists(path):
+            raise FileNotFoundError()
+
+        self._path = path
+        self._serializer = serializer
+
+        self._data: dict[str, Record]
+        self._name: str
+
+        self._name, self._data = map(
+            ISerializable.deserialize,
+            serializer.deserialize_list(path))
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def path(self) -> pathlib.Path:
+        return self._path

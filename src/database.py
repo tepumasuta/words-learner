b5669931868654da9fba5ae9a661da50f358bd4a
tepumasuta <thinkingocean2@gmail.com>
2022-07-26 19:33:53 +0300

    Implemented `ISerializer` interface in `Record` dataclass

--- src/database.py
+++ src/database.py
@@ -1,5 +1,5 @@
 import datetime
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, asdict
 from serialize import *
 
 
@@ -9,3 +9,13 @@ class Record(ISerializable):
     contents: list[str] = field(default_factory=list)
     last_update_date: datetime.date = field(default_factory=datetime.date.today)
     repeated_times: int = 0
+
+    def serialize(self) -> dict:
+        return asdict(self)
+
+    @staticmethod
+    def deserialize(raw: dict) -> 'Record':
+        return Record(raw['mapped_word'],
+                      raw['contents'],
+                      raw['last_update_date'],
+                      raw['repeated_times'])

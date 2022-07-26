512df71af4e2016819cc351607644044449cebd1
FedNirvanous451 <teoforzur@gmail.com>
2022-07-26 19:56:59 +0300

    Fix test_method.py: add Database

--- src/test_method.py
+++ src/test_method.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
 
 from action import IAction
+from database import Database
 
 class ITestmethod(ABC):
     @abstractmethod
-    def test(self) -> IAction:
+    def test(self, data: Database) -> IAction:
         ...

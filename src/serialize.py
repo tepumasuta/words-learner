9a8f43a953a2c8d099d17c944d997f4167c1f98c
tepumasuta <thinkingocean2@gmail.com>
2022-07-26 20:23:13 +0300

    Implemeted `PickleSerializer`

--- src/serialize.py
+++ src/serialize.py
@@ -1,5 +1,7 @@
-from abc import ABC, abstractmethod
 import pathlib
+import os
+import pickle
+from abc import ABC, abstractmethod
 
 
 class ISerializable(ABC):
@@ -23,3 +25,27 @@ class ISerializer(ABC):
     @abstractmethod
     def deserialize_list(path: pathlib.Path) -> list[ISerializable]:
         ...
+
+
+class PickleSerializer(ISerializer):
+    @staticmethod
+    def serialize_list(objs: list[ISerializable | str], path: pathlib.Path):
+        def is_special(obj) -> bool:
+            return isinstance(obj, str)
+        
+        if not os.path.exists(path):
+            raise OSError()
+        
+        with open(path) as out:
+            pickle.dump([obj.serialize() if not is_special(obj) else obj for obj in objs], out)
+    
+    @staticmethod
+    def deserialize_list(path: pathlib.Path) -> list[ISerializable]:
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"File `{path}` not found")
+        
+        res: list[ISerializable]
+        with open(path) as data:
+            res = pickle.load(data)
+
+        return res

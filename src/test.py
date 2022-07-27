deda43f270f5b541e330128b9aed1002bdef7b81
tepumasuta <thinkingocean2@gmail.com>
2022-07-27 19:22:10 +0300

    Renamed fields in private convention

--- src/test.py
+++ src/test.py
@@ -14,19 +14,19 @@ class BasicTestmethod(ITestmethod):
     KEYS_AMOUNT = 20
     
     def __init__(self):
-        self.amount_keys = BasicTestmethod.KEYS_AMOUNT
-        self.list_keys = []
+        self._amount_keys = BasicTestmethod.KEYS_AMOUNT
+        self._list_keys = []
 
     def test(self, data: Database) -> TestAction:
-        while self.amount_keys != 0:
-            if len(self.list_keys) <= BasicTestmethod.KEYS_AMOUNT:
+        while self._amount_keys != 0:
+            if len(self._list_keys) <= BasicTestmethod.KEYS_AMOUNT:
                 break
 
             ran_choice = random.choice(data.keys())
-            if ran_choice not in self.list_keys:
-                self.list_keys.append(ran_choice)
+            if ran_choice not in self._list_keys:
+                self._list_keys.append(ran_choice)
                 
-                if self.amount_keys > len(data.keys()):
-                    self.amount_keys -= 1
+                if self._amount_keys > len(data.keys()):
+                    self._amount_keys -= 1
 
-        return TestAction(self.list_keys)
+        return TestAction(self._list_keys)

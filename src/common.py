7c6b3196c9105a21c3c1bc89e5984ac60210f44f
tepumasuta <thinkingocean2@gmail.com>
2022-08-21 18:05:31 +0300

    Fixed typo

--- src/common.py
+++ src/common.py
@@ -3,4 +3,4 @@ from typing import Any
 def _type_check(*params: tuple[Any, type, str, str]):
     for var, var_type, var_name, var_type_name in params:
         if not isinstance(var, var_type):
-            raise TypeError(f"{var_name} must be {var_type_name}. Recieved `{var}` of type {type(var)}")
+            raise TypeError(f"{var_name} must be {var_type_name}. Received `{var}` of type {type(var)}")
